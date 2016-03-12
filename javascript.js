37fe8f42edb6f74fb0c219ca8ded82740bc8258b
Amanda <nadouamanda@icloud.com>
2016-03-12 15:48:16 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -23,6 +23,7 @@ angular.module('portalApp')
         var sogo=$sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d92664.78523735823!2d-80.59420960287662!3d43.46530398904281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf4013bc9bc25%3A0x3e0668302d13b35f!2sSogo!5e0!3m2!1sen!2sca!4v1457815152390");
         var kenzo=$sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4039616083387!2d-80.53756514863863!3d43.4730393790255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf407ca1e311d%3A0x237a7a97779ab8cb!2sKenzo+Ramen!5e0!3m2!1sen!2sca!4v1457807372264");
         var williams=$sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11581.615914727157!2d-80.5441580433868!3d43.4730390226613!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf401323dd441%3A0x93dc4effd98a9bc8!2sWilliams+Fresh+Cafe!5e0!3m2!1sen!2sca!4v1457815484064");
+        var nuri=$sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4668380877815!2d-80.54099904863871!3d43.47172697902547!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf4013bc9bc25%3A0x525618b4173fd8a8!2sNuri+Village!5e0!3m2!1sen!2sca!4v1457815604044");
         // Model for the search and list example
         $scope.model = [{
             title: "Waterloo Star",
@@ -53,7 +54,7 @@ angular.module('portalApp')
             address: "170 University Avenue West, Waterloo, ON, Canada",
             style: "Eastern",
             ratings: '5',
-            mapurl: test
+            mapurl: nuri
         }, {
             title: "Mr. Sushi",
             address: "170 University Avenue West, Waterloo, ON, Canada",
