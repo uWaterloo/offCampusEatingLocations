78a5c810cb00ddb80bef1c0f534e1794fa2fdd4c
Amanda <nadouamanda@icloud.com>
2016-03-12 15:38:07 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -19,12 +19,14 @@ angular.module('portalApp')
         offCampusEatingLocationsFactory.init($scope);
 
     	var test=$sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4146072148083!2d-80.53988384863868!3d43.47281717902555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf401323dd441%3A0xc2bc480b90231754!2sWaterloo+Star!5e0!3m2!1sen!2sca!4v1457803922857");
+        var waterlooStar=$sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4146072148083!2d-80.53988384863868!3d43.47281717902555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf401323dd441%3A0xc2bc480b90231754!2sWaterloo+Star!5e0!3m2!1sen!2sca!4v1457803922857");
         // Model for the search and list example
         $scope.model = [{
             title: "Waterloo Star",
             address: "170 University Avenue West, Waterloo, ON, Canada",
             style: "Eastern",
-            ratings: '5'
+            ratings: '5',
+            mapurl: waterlooStar
         }, {
             title: "Sogo",
             address: "170 University Avenue West, Waterloo, ON, Canada",
