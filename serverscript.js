8fef84810eb4fc1900eaae4f9edc2cfdb6aebf86
Veronica <VeronicaHuang@users.noreply.github.com>
2016-03-12 11:23:51 -0500

    Updated user branch from Portal workspace (server script)

--- serverscript.js
+++ serverscript.js
@@ -1,3 +1,16 @@
+// Retreive data from the database
+function getData() {
+    var queryResult = db.Execute('SELECT * FROM sampleTable');
+    var rows = JSON.parse(queryResult);
+    if (rows.length > 0 && typeof rows[0].Error != 'undefined') {
+        return '{"status":"noTable"}';
+    }
+    return queryResult;
+}
+
+
+
+
 // Insert into the database
 function insert() {
     if (args.Get("value").length > 50)
