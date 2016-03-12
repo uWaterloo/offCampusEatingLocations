cc9d5ec4bf0164c8c9ea6e7e57dc498786b3d1e9
Amanda <nadouamanda@icloud.com>
2016-03-12 13:16:04 -0500

    Updated user branch from Portal workspace (javascript.js)

--- javascript.js
+++ javascript.js
@@ -22,6 +22,7 @@ offCampusEatingLocationsFactory) {
         details: "Waterloo Star details",
         foodType: "Eastern",
         location: "UW plaza"
+        mapurl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2895.4146072148083!2d-80.53988384863868!3d43.47281717902555!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x882bf401323dd441%3A0xc2bc480b90231754!2sWaterloo+Star!5e0!3m2!1sen!2sca!4v1457803922857"
     }, {
         title: "Sogo",
         details: "Sogo details",
